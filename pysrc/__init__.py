f84acfa9ae39b924f369f9ec6505885978c13b73
learnforpractice <learnforpractice@gmail.com>
2023-05-24 19:16:00 +0800

    Bump version to v0.2.7

--- pysrc/__init__.py
+++ pysrc/__init__.py
@@ -9,7 +9,7 @@ import shutil
 import argparse
 from .wasm_checker import check_import_section
 
-__version__ = "0.2.6"
+__version__ = "0.2.7"
 
 src_dir = os.path.dirname(__file__).replace('\\', '/')
 
--- tag.sh
+++ tag.sh
@@ -1,4 +1,4 @@
-VERSION=v0.2.6
+VERSION=v0.2.7
 TARGET=origin
 git push $TARGET :refs/tags/$VERSION
 git tag -d $VERSION
